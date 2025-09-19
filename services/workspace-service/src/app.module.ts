ff321b59b8b0c3c5551f29b8d0b7eaa72a23d031
Illia Skaryna <subscriber6436@gmail.com>
2025-09-19 22:42:04 +0300

    fix(workspace): minor edit

--- services/workspace-service/src/app.module.ts
+++ services/workspace-service/src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { BoardsModule } from './modules/boards/boards.module';
 import { ElementsModule } from './modules/elements/elements.module';
-import { EventsModule } from './modules/events/events.module';
 import { ProjectsModule } from './modules/projects/projects.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
 
 @Module({
-  imports: [ProjectsModule, BoardsModule, PrismaModule, ElementsModule, EventsModule],
+  imports: [ProjectsModule, BoardsModule, PrismaModule, ElementsModule],
 })
 export class AppModule {}
